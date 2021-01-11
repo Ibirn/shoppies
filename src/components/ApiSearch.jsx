5ea2d4c93874291c61ad1b45cadbe22670e43d1a
Ibirn <kmcricciuti@gmail.com>
2021-01-11 10:31:54 -0800

    Add axios GET for OMDB

---
+++ src/components/ApiSearch.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function ApiSearch(props) {
+  //set default query to empty
+  //http://www.omdbapi.com/?apikey=${REACT_APP_OMDB_KEY}&
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  // console.log(process.env);
+
+  const dbSearch = (e) => {
+    e.preventDefault();
+    console.log();
+    axios
+      .get(
+        `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&s=${query}`
+      )
+      .then((response) => setResults(response.data));
+  };
+
+  return (
+    <div>
+      <form action="" method="get" className="form-example">
+        <div className="form-example">
+          <label>Search OMDB: </label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+            value={query}
+            required
+          ></input>
+          <button onClick={(e) => dbSearch(e)}>Search</button>
+        </div>
+      </form>
+    </div>
+  );
+}
