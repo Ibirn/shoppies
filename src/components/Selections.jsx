1c9bc307a58566e6a35b879f456c8003f35e3481
Ibirn <kmcricciuti@gmail.com>
2021-01-12 17:08:57 -0800

    Bugfix duplicates, allow for rearranging of selected movies

--- src/components/Selections.jsx
+++ src/components/Selections.jsx
@@ -4,7 +4,7 @@ import "../styles/nominationStyle.scss";
 export default function Selections({ choice }) {
   console.log("SELEC: ", choice);
   return (
-    <div className="nomination-choice slot" draggable={true}>
+    <div className="nomination-choice" draggable={true}>
       <img src={choice.Poster} alt={`${choice.Title}-poster`}></img>
       <span>
         <h3>{choice.Title}</h3>
--- src/hooks/useDragAndDrop.jsx
+++ src/hooks/useDragAndDrop.jsx
@@ -1,71 +1,109 @@
 import { useEffect, useState } from "react";
 
 export default function useDragAndDrop(props) {
-  const [nominations, setNominations] = useState([]);
-
-  // console.log("D&D loaded", props);
+  const [nominations, setNominations] = useState([
+    // {
+    //   Title: "Nick Offerman: American Ham",
+    //   Year: "2014",
+    //   imdbID: "tt3441700",
+    //   Type: "movie",
+    //   Poster:
+    //     "https://m.media-amazon.com/images/M/MV5BMDdjZTFlYjItYTUxZi00NDVlLWEyOGYtMjU3OWJmYjMxMTM0XkEyXkFqcGdeQXVyMTk3NDAwMzI@._V1_SX300.jpg",
+    // },
+    // {
+    //   Title: "Ham & Cheese",
+    //   Year: "2004",
+    //   imdbID: "tt0342505",
+    //   Type: "movie",
+    //   Poster:
+    //     "https://m.media-amazon.com/images/M/MV5BMTQ2OTY3NTYwNF5BMl5BanBnXkFtZTgwMDI4OTk1MDE@._V1_SX300.jpg",
+    // },
+  ]);
 
   let dragSource = null;
-
-  useEffect(() => {
-    console.log("RC: ", props.results);
-  }, [props.results]);
+  let dragSwap = null;
 
   function handleDragStart(e) {
     dragSource = this;
-    e.dataTransfer.effectAllowed = "copy";
-    e.dataTransfer.setData(
-      "text/html",
-      JSON.stringify(props.results[this.id[0]])
-    );
-    // console.log("DS: ", this, props.results[this.id[0]]);
+    e.dataTransfer.effectAllowed = "copyMove";
+    if (this.className.includes("search-result")) {
+      e.dataTransfer.setData(
+        "text/html",
+        JSON.stringify(props.results[this.id[0]])
+      );
+    } else {
+      e.dataTransfer.setData("text/html", this.innerHTML);
+    }
   }
 
   function handleDragOver(e) {
     if (e.preventDefault) {
       e.preventDefault();
     }
-    e.dataTransfer.dropEffect = "copy";
+    if (this.className.includes("nominations")) {
+      e.dataTransfer.dropEffect = "copy";
+    } else {
+      dragSwap = this;
+      e.dataTransfer.dropEffect = "move";
+    }
+
     return false;
   }
 
   function handleDrop(e) {
-    console.log("HD DS: ", dragSource);
-    console.log("HD TH: ", this);
     if (dragSource !== this && dragSource !== null) {
-      // document.getElementById("drop-zone").appendChild();
-      // dragSource.innerHTML = this.innerHTML;
-      setNominations((prev) => [
-        ...prev,
-        JSON.parse(e.dataTransfer.getData("text/html")),
-      ]);
+      if (dragSwap && dragSource.className.includes("nomination-choice")) {
+        dragSource.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData("text/html");
+        e.stopPropagation();
+      } else {
+        setNominations((prev) => [
+          ...prev,
+          JSON.parse(e.dataTransfer.getData("text/html")),
+        ]);
+        e.stopPropagation();
+      }
     }
+    e.dataTransfer.clearData("text/html");
   }
 
   useEffect(() => {
     //grab all items
     console.log("GOING");
     let items = document.querySelectorAll(".slot");
-    let selections = document.getElementById("drop-zone");
+    let dropZone = document.getElementById("drop-zone");
+    let nominees = document.querySelectorAll(".nomination-choice");
     //give each item listeners for drag
     items.forEach((item) => {
       item.addEventListener("dragstart", handleDragStart);
-      // item.addEventListener("drop", handleDrop);
       item.addEventListener("dragover", handleDragOver);
     });
-    console.log("SELECTIONS: ", selections);
-    selections.addEventListener("dragover", handleDragOver);
-    selections.addEventListener("drop", handleDrop);
+
+    nominees.forEach((nominee) => {
+      nominee.addEventListener("dragstart", handleDragStart);
+      nominee.addEventListener("dragover", handleDragOver);
+      nominee.addEventListener("drop", handleDrop);
+      console.log("APPLIED: ", nominee);
+    });
+
+    dropZone.addEventListener("dragover", handleDragOver);
+    dropZone.addEventListener("drop", handleDrop);
 
     return () => {
       //remove the goddamn listeners to prevent problems, you numpty - 5 hours and you should know better.
       items.forEach((item) => {
         item.removeEventListener("dragstart", handleDragStart);
-        // item.removeEventListener("drop", handleDrop);
         item.removeEventListener("dragover", handleDragOver);
       });
-      selections.removeEventListener("dragover", handleDragOver);
-      selections.removeEventListener("drop", handleDrop);
+
+      nominees.forEach((nominee) => {
+        nominee.removeEventListener("dragstart", handleDragStart);
+        nominee.removeEventListener("dragover", handleDragOver);
+        nominee.removeEventListener("drop", handleDrop);
+      });
+
+      dropZone.removeEventListener("dragover", handleDragOver);
+      dropZone.removeEventListener("drop", handleDrop);
 
       console.log("GONE");
     };
